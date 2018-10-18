32d9cabf27423218b57ccd0480d74e798e0d3fc0
Umherirrender <umherirrender_de.wp@web.de>
2018-10-18 21:23:38 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: If48fd3a439535868f06b73c8b26bb7d65ba9cfb9

--- Insider.php
+++ Insider.php
@@ -4,10 +4,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['Insider'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['InsiderMagic'] = __DIR__ . '/Insider.i18n.magic.php';
-	/*wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for Insider extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	);*/
+	);
 	return;
 } else {
 	die( 'This version of the Insider extension requires MediaWiki 1.25+' );
